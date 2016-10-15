f4cba754b03a3894d9e5882c558dd0b8026b39fe
alblum528 <alblum528@gmail.com>
2016-10-15 18:15:06 +0200

    Originals

---
+++ site.js
@@ -0,0 +1,9 @@
+// // // var text = prompt("say what!!!")
+// // // alert(text)
+// // var saveBtn = document.getElementById("Save");
+// // saveBtn.onclick = function saveTheWork(event) {
+// // 	// body...
+// // 	saveBtn.disabled = true;
+// // 	alert("Saved Successfully");
+
+// }
